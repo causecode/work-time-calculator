613722b17abc6ead3685996c353f8bcd59d7f016
Shashank Agrawal <shashank.agrawal@causecode.com>
2015-10-12 22:19:39 +0530

    Fixed edge conditions for calculating minutes.

--- src/main/java/com/cc/BusinessTimeCalculator.java
+++ src/main/java/com/cc/BusinessTimeCalculator.java
@@ -5,6 +5,11 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+/*
+ * A utility class to calculate business minutes/hours/days between two given dates.
+ * @since 0.0.1
+ * @auhor Shashank Agrawal
+ */
 class BusinessTimeCalculator {
 
     private Date startDate;
@@ -17,12 +22,25 @@ class BusinessTimeCalculator {
     private String dayStartTimeString = "09:30", dayEndTimeString = "18:30";
     private DateFormat timeParser = new SimpleDateFormat("HH:mm");
 
+    private int weekendDay1 = Calendar.SUNDAY;
+    private int weekendDay2 = Calendar.SATURDAY;
+
     BusinessTimeCalculator(Date startDate, Date endDate) {
         this.startDate = startDate;
         this.endDate = endDate;
 
         startCal.setTime(startDate);
         endCal.setTime(endDate);
+    }
+
+    private void validateWeekend(int weekendDay) {
+        if (weekendDay < 0 && weekendDay > Calendar.SATURDAY) {
+            throw new IllegalArgumentException("Invalid weekend day selected");
+        }
+    }
+
+    private void log(Object data) {
+        System.out.println(data);
     }
 
     private Boolean isInWorkingDay() {
@@ -44,17 +62,38 @@ class BusinessTimeCalculator {
             calendar2.set(field, startCal.get(field));
         }
 
+        // Get the work start time of the date
         Date date1 = calendar1.getTime();
+        // Get the work end time of the date
         Date date2 = calendar2.getTime();
         Date date = startCal.getTime();
 
-        return date.after(date1) && date.before(date2);
+        if (date1.before(startDate)) {
+            date1 = startDate;
+        }
+
+        // If the work end time is greater than the endDate
+        if (date2.after(endDate)) {
+            // Then set it to the endDate to stop calculating further and also fix invalid calculation
+            date2 = endDate;
+        }
+
+        // Consider the start time as inclusive and end date as exclusive
+        Boolean status = (date.equals(date1) || date.after(date1)) && date.before(date2);
+        log("" + date1 + " > " + date + " < " + date2 + " " + status);
+        return status;
     }
 
-    Double getDays() {
+    Integer getMinutes() {
+        // TODO Optimize this loop
         while (startCal.getTimeInMillis() <= endCal.getTimeInMillis()) {
             int day = startCal.get(Calendar.DAY_OF_WEEK);
-            if (day == Calendar.SATURDAY || day == Calendar.SUNDAY || !isInWorkingDay()) {
+            if (day == Calendar.SATURDAY || day == Calendar.SUNDAY) {
+                startCal.add(Calendar.DAY_OF_MONTH, 1);
+                continue;
+            }
+
+            if (!isInWorkingDay()) {
                 startCal.add(Calendar.MINUTE, 1);
                 continue;
             }
@@ -63,6 +102,19 @@ class BusinessTimeCalculator {
             startCal.add(Calendar.MINUTE, 1);
         }
 
+        return minutes;
+    }
+
+    Double getDays() {
+        getMinutes();
         return (double) (minutes / (60 * 8));
+    }
+
+    void setWeekends(int weekendDay1, int weekendDay2) {
+        validateWeekend(weekendDay1);
+        validateWeekend(weekendDay2);
+
+        this.weekendDay1 = weekendDay1;
+        this.weekendDay2 = weekendDay2;
     }
 }
--- src/test/java/com/cc/BusinessTimeCalculatorTest.java
+++ src/test/java/com/cc/BusinessTimeCalculatorTest.java
@@ -12,6 +12,8 @@ public class BusinessTimeCalculatorTest {
 
     private static DateFormat parser = new SimpleDateFormat("MM/dd/yyyy HH:mm");
 
+    private BusinessTimeCalculator calculator;
+
     private Date parse(String date) {
         try {
             return parser.parse(date);
@@ -21,11 +23,27 @@ public class BusinessTimeCalculatorTest {
     }
 
     @Test
-    public void testCalculatorForVariousChecks() {
-        BusinessTimeCalculator calculator = new BusinessTimeCalculator(parse("05/01/2015 09:30"), parse("05/01/2015 " +
-                "18:29"));
+    public void testCalculatorForOneCompleteWorkingDay() {
+        calculator = new BusinessTimeCalculator(parse("05/01/2015 09:30"), parse("05/01/2015 18:30"));
 
         Assert.assertEquals(1d, calculator.getDays(), 0);
+        Assert.assertEquals(540, calculator.getMinutes(), 0);
+    }
+
+    @Test
+    public void testCalculatorForOneDay() {
+        calculator = new BusinessTimeCalculator(parse("05/01/2015 09:30"), parse("05/02/2015 09:30"));
+
+        Assert.assertEquals(1d, calculator.getDays(), 0);
+        Assert.assertEquals(540, calculator.getMinutes(), 0);
+    }
+
+    @Test
+    public void testCalculatorForOneDayIncludingTwoDaysAndSaturdaySunday() {
+        calculator = new BusinessTimeCalculator(parse("05/01/2015 12:30"), parse("05/04/2015 12:30"));
+
+        Assert.assertEquals(1d, calculator.getDays(), 0);
+        Assert.assertEquals(540, calculator.getMinutes(), 0);
     }
 
 }
